1fee3ed4379360d6b55a42f31fd5797d01acf0ae
gaeng2y <gaeng2y@gmail.com>
2023-01-04 23:42:00 +0900

    2023 01 04

---
+++ HelloPublishers2/HelloPublishers/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  HelloPublishers
+//
+//  Created by Mohammad Azam on 9/1/19.
+//  Copyright © 2019 Mohammad Azam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let notification = Notification.Name("MyNotification")
+        
+        let publisher = NotificationCenter.default.publisher(for: notification, object: nil)
+        
+        let subscription = publisher.sink { _ in
+                  print("Notification recevied")
+        }
+        
+        NotificationCenter.default.post(name: notification, object: nil)
+        
+      
+       
+    }
+
+
+}
+
