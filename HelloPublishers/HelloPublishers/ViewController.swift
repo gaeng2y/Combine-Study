705834c965a993c775298e2f0e068a8cd2845bd1
gaeng2y <gaeng2y@gmail.com>
2023-01-07 23:12:51 +0900

    2023 01 07

--- HelloPublishers/HelloPublishers/ViewController.swift
+++ HelloPublishers/HelloPublishers/ViewController.swift
@@ -7,28 +7,43 @@
 //
 
 import UIKit
+import Combine
+
+class StringSubscriber: Subscriber {
+    
+    func receive(subscription: Subscription) {
+        print("Received Subscription")
+        subscription.request(.max(3)) // backpressure
+    }
+    
+    func receive(_ input: String) -> Subscribers.Demand {
+        print("Received Value", input)
+        return .unlimited
+    }
+    
+    func receive(completion: Subscribers.Completion<Never>) {
+        print("Completed")
+    }
+    
+    
+    typealias Input = String
+    typealias Failure = Never
+    
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let notification = Notification.Name("MyNotification")
+        let publisher = ["A","B","C","D","E","F","G","H","I","J","K"].publisher
         
-        let center = NotificationCenter.default
+        let subscriber = StringSubscriber()
         
-        let observer = center.addObserver(forName: notification, object: nil, queue: nil) { notification in
-            print("Notification recevied!")
-        }
-        
-        center.post(name: notification, object: nil)
-        
-        center.removeObserver(observer)
- 
-        
+        publisher.subscribe(subscriber)
         
     }
-
+   
 
 }
 
